79c4d1e7732f2d667ab00d94cbb820069f3e2da8
Amine_B <aminelous1@gmail.com>
2025-05-04 17:59:10 +0100

    pushing before merging

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2ece4742af6a43427b80e45a41970a9fc95502e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44514aaf131be5cc11fc8b8517a69217326dee23")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
