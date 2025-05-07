5f47f50458cf9cbafca2f146740ca2cf10475560
Amine_B <aminelous1@gmail.com>
2025-05-07 09:47:51 +0100

    downgrading codeGeneration.Design from 9 to 8.0.7 to fix version error

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c4d1e7732f2d667ab00d94cbb820069f3e2da8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51423b978b3a5e3eb174a413ea11d9b726cfeb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
