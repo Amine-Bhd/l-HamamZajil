abe0cf8a14e03839ee2bb8f541e57bf645f0d2a3
Amine_B <aminelous1@gmail.com>
2025-05-09 23:39:03 +0100

    adding users to database, database user populating

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4b3d4cc2ee4f9317c29f459ed713a711607db85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bf201d2faff060a816cec1d8e42eb73d31af03f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
